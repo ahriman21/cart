16083ee9ba0d1c02bee6d7ba4f2c4a946b72ed53
ahriman <109135045+ahriman21@users.noreply.github.com>
2023-01-17 22:04:45 +0330

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,3 +4,14 @@ from django import forms
 class AddToCartForm(forms.Form):
     quantity = forms.IntegerField(widget=forms.NumberInput(),initial=1,label='تعداد',min_value=1,max_value=10)
     product_id = forms.IntegerField(widget=forms.HiddenInput())
+
+
+# use in product-detail-view.html :
+
+  <form action="{% url 'cart:add' %}" method="post">
+    {% csrf_token %}
+    {{add_to_cart_form}}
+     <div >
+        <input type="submit" value="افزودن به سبدخرید" class="btn-single" >
+    </div>
+ </form>
